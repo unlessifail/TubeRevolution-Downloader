a0e13c2d926943f9eb51cb9f28f2a2914454465c
Kalil M. Santos <34076730+unlessifail@users.noreply.github.com>
2023-11-12 17:40:18 -0300

    Arquivos Subidos, V1.0
    App baixando em MP3, MP4 E WAV

---
+++ main.py
@@ -0,0 +1,109 @@
+import os
+import tkinter as tk
+from tkinter import ttk
+from PIL import Image, ImageTk
+from pytube import YouTube
+from pytube.exceptions import RegexMatchError
+
+class TubeRevolution:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("TubeRevolution Downloader")
+        self.master.geometry("400x200")
+        self.master.configure(bg='#eb4034')
+
+        self.create_widgets()
+
+    def create_widgets(self):
+        # Criação dos widgets
+        self.url_label = tk.Label(self.master, bg="yellow", text="URL do YouTube:")
+        self.url_label.pack()
+
+        self.url_entry = tk.Entry(self.master, width=40)
+        self.url_entry.pack()
+
+        self.format_label = tk.Label(self.master, bg="yellow", text="Baixar como:")
+        self.format_label.pack()
+
+        self.format_var = tk.StringVar()
+        self.format_var.set("mp4")
+
+        # Adicionando "wav" à lista de opções
+        self.format_options = ["mp3", "mp4", "wav"]
+        self.format_menu = tk.OptionMenu(self.master, self.format_var, *self.format_options)
+        self.format_menu.pack()
+
+        # Carregar a imagem 'icon.png'
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        icon_path = os.path.join(script_dir, "icon.png")
+
+        try:
+            icon = Image.open(icon_path)
+            icon = ImageTk.PhotoImage(icon)
+            self.icon_label = tk.Label(self.master, image=icon, bg='#eb4034')
+            self.icon_label.image = icon
+            self.icon_label.place(x=50, y=100)
+
+        except FileNotFoundError:
+            print("Arquivo 'icon.png' não encontrado.")
+
+        self.progress_bar = ttk.Progressbar(self.master, orient=tk.HORIZONTAL, length=400, mode='determinate')
+        self.progress_bar.pack()
+
+        self.download_button = tk.Button(self.master, bg="yellow", text="Baixar", command=self.download)
+        self.download_button.pack()
+
+    def on_progress(self, stream, chunk, remaining):
+        file_size = stream.filesize
+        downloaded = file_size - remaining
+        progress = downloaded / file_size * 100.0
+        self.progress_bar['value'] = progress
+        self.master.update()
+
+    def download(self):
+        url = self.url_entry.get()
+        format_option = self.format_var.get()
+        download_path = os.path.join(os.path.expanduser("~"), "Desktop")
+
+        try:
+            yt = YouTube(url)
+
+            if format_option == "mp3":
+                audio_stream = yt.streams.filter(only_audio=True).first()
+                self.download_stream(audio_stream, download_path, f"{yt.title}.mp3")
+            elif format_option == "mp4":
+                video_stream = yt.streams.filter(file_extension=format_option, progressive=True).first()
+                self.download_stream(video_stream, download_path, f"{yt.title}.{format_option}")
+            elif format_option == "wav":
+                # Adiciona uma nova cláusula `else` para baixar em WAV
+                audio_stream = yt.streams.filter(only_audio=True).first()
+                self.download_stream(audio_stream, download_path, f"{yt.title}.wav")
+            else:
+                print("Formato não suportado.")
+
+        except RegexMatchError:
+            print("URL do YouTube inválida.")
+        except Exception as e:
+            print(f"Erro: {str(e)}")
+
+    def download_stream(self, stream, download_path, filename):
+
+        if stream:
+
+            self.progress_bar['value'] = 0
+            self.master.update()
+
+            self.progress_bar['value'] = 20
+            stream.download(download_path)
+
+            os.rename(os.path.join(download_path, stream.default_filename), os.path.join(download_path, filename))
+
+            self.progress_bar['value'] = 100
+            print("Download concluído!")
+        else:
+            print(f"Nenhuma stream encontrada para o formato {self.format_var.get()}.")
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = TubeRevolution(root)
+    root.mainloop()
